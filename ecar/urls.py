ed8301da7aeed1ba8306763ef58f5bfe39478ae5
Shrutipavasiya <shrutipavasiya17012002@gmail.com>
2023-04-15 14:22:09 +0530

    error

--- ecar/urls.py
+++ ecar/urls.py
@@ -21,4 +21,6 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('pages.urls')),
+    path('cars/',include('cars.urls')),
+    path("ckeditor/", include('cars.ckeditor_uploader.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
